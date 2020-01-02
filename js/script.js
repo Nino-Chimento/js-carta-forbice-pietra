76653c00cc9f3b2d135b982ac83e9211668e301b
Nino Chimento <saninoc84@gmail.com>
2020-01-02 15:18:48 +0100

    seconda condizione carta

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var carta = document.getElementById('carta-player');
 var forbice = document.getElementById('forbice-player');
 var pietra = document.getElementById('pietra-player');
-var cartaPc = document.getElementById('carta-cpu');
+var cartaCpu = document.getElementById('carta-cpu');
 var forbiceCpu = document.getElementById('forbice-cpu');
 var pietraCpu = document.getElementById('pietra-cpu');
-var scorePlayer = document.getElementById("score-player");
-var scoreCpu = document.getElementById('score-cpu');
+var scorePlayer = 0;
+var scoreCpu = 0;
 var sceltaPc =  Math.floor(Math.random() * 3 + 1);
 console.log(sceltaPc);
 // specifico cosa succede ogni volta che si clicca su uan scelta
@@ -14,11 +14,23 @@ function() {
   forbice.classList.add("hidden");
   pietra.classList.add("hidden");
   carta.classList.add("animation")
-  if (sceltaPc== 1) {
-    pietraPc.classList.add("hidden");
-    forbicePc.classList.add("hidden");
-    forbicePc.classList.add("animation");
+  if (sceltaPc == 1) {
+    pietraCpu.classList.add("hidden");
+    forbiceCpu.classList.add("hidden");
+    cartaCpu.classList.add("animation-pc");
+    var risultato = "pareggio";
+    document.getElementById('risultato').innerHTML= risultato
   }
+  else if (sceltaPc == 2) {
+    pietraCpu.classList.add("hidden");
+    forbiceCpu.classList.add("animation-pc");
+    cartaCpu.classList.add("hidden");
+    var risultato = "vittoriaPc";
+    scoreCpu++;
+    document.getElementById('risultato').innerHTML= risultato;
+    document.getElementById('score-cpu').innerHTML = scoreCpu;
+  }
+
 
 
 
